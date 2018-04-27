cfb26c43f9a762efcfdb5e23e5f4c7dbddfc9692
Renato Samperio <renatosmaperio@gmail.com>
2018-04-27 22:53:29 +0200

    PY_SETUP: Changed utils path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['utils'],
+    packages=['hs_utils'],
     package_dir={'' : 'src'},
 )
 
