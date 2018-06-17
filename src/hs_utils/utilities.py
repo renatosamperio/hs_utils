a52fde693a1a590b4ae97cbbc2b59da12c6be7e1
Renato Samperio <renatosamperio@gmail.com>
2018-06-17 20:57:54 +0200

    SIMILARITY: Removed imports and changed print

--- src/hs_utils/utilities.py
+++ src/hs_utils/utilities.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
-import rospy
 import sys, os
 import threading
 import time
-
-from optparse import OptionParser
-from std_msgs.msg import Bool
-from std_msgs.msg import String
 
 def ParseException(inst):
   ''' Takes out useful information from incoming exceptions'''
@@ -16,7 +11,7 @@ def ParseException(inst):
   exception_line = str(exc_tb.tb_lineno) 
   exception_type = str(type(inst))
   exception_desc = str(inst)
-  rospy.logerr( "  %s: %s in %s:%s"%(exception_type, 
+  print( "  %s: %s in %s:%s"%(exception_type, 
                 exception_desc, 
                 exception_fname,  
                 exception_line ))
