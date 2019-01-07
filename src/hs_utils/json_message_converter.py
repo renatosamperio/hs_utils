76b5f0d1cd2025e753bd1b36fe7bebbac640a0d0
Renato Samperio <renatosamperio@gmail.com>
2019-01-07 22:01:16 +0100

    JSON_MSG_CONVERTER: Added debug flag

--- src/hs_utils/json_message_converter.py
+++ src/hs_utils/json_message_converter.py
@@ -14,7 +14,7 @@ def convert_json_to_ros_message(message_type, json_message):
     dictionary = json.loads(json_message)
     return message_converter.convert_dictionary_to_ros_message(message_type, dictionary)
 
-def convert_ros_message_to_json(message):
+def convert_ros_message_to_json(message, debug=False):
     """
     Takes in a ROS message and returns a JSON-formatted string.
 
@@ -22,6 +22,6 @@ def convert_ros_message_to_json(message):
         ros_message = std_msgs.msg.String(data="Hello, Robot")
         json_message = convert_ros_message_to_json(ros_message)
     """
-    dictionary = message_converter.convert_ros_message_to_dictionary(message)
+    dictionary = message_converter.convert_ros_message_to_dictionary(message, debug=debug)
     json_message = json.dumps(dictionary)
     return json_message
