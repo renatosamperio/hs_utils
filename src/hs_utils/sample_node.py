2d9e791e475fb931427ad43e1db45a2141c32b8e
Renato Samperio <renato@sunflower-labs.com>
2019-05-11 19:07:18 +0200

    SAMPLE_NODE: Added file

---
+++ src/hs_utils/sample_node.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import sys, os
+import threading
+import rospy
+import datetime
+import time
+import json
+
+from optparse import OptionParser, OptionGroup
+from pprint import pprint
+
+from hs_utils import ros_node, logging_utils
+from hs_utils import message_converter as mc
+from hs_utils import json_message_converter as rj
+from hs_utils.mongo_handler import MongoAccess
+#from events_msgs.msg import WeeklyEvents
+
+class Sample(ros_node.RosNode):
+    def __init__(self, **kwargs):
+        try:
+            
+            ## Initialising parent class with all ROS stuff
+            super(Sample, self).__init__(**kwargs)
+            
+            ## Initialise node activites
+            self.Init()
+        except Exception as inst:
+              ros_node.ParseException(inst)
+              
+    def SubscribeCallback(self, msg, topic):
+        try:
+            message = msg
+            
+        except Exception as inst:
+              ros_node.ParseException(inst)
+      
+    def Init(self):
+        try:
+            rospy.Timer(rospy.Duration(10.0), self.Run)
+        except Exception as inst:
+              ros_node.ParseException(inst)
+              
+    def ShutdownCallback(self):
+        try:
+            rospy.logdebug('+ Shutdown: Doing nothing...')
+        except Exception as inst:
+              ros_node.ParseException(inst)
+              
+    def Run(self, event):
+        ''' Run method '''
+        try:
+            rospy.logdebug('+ Publishing data')
+            
+        except Exception as inst:
+              ros_node.ParseException(inst)
+
+
+if __name__ == '__main__':
+    usage       = "usage: %prog option1=string option2=bool"
+    parser      = OptionParser(usage=usage)
+    parser.add_option('--queue_size',
+                type="int",
+                action='store',
+                default=1000,
+                help='Topics to play')
+    parser.add_option('--latch',
+                action='store_true',
+                default=False,
+                help='Message latching')
+    parser.add_option('--debug',
+                action='store_true',
+                default=False,
+                help='Provide debug level')
+    parser.add_option('--syslog',
+                action='store_true',
+                default=False,
+                help='Start with syslog logger')
+
+    (options, args) = parser.parse_args()
+    
+    args            = {}
+    logLevel        = rospy.DEBUG if options.debug else rospy.INFO
+    rospy.init_node('band_search', anonymous=False, log_level=logLevel)
+    
+    ## Sending logging to syslog
+    if options.syslog:
+        logging_utils.update_loggers()
+
+    ## Defining static variables for subscribers and publishers
+    sub_topics     = [
+#         ('/event_locator/weekly_events',  WeeklyEvents),
+    ]
+    pub_topics     = [
+#         ('/event_locator/updated_events', WeeklyEvents)
+    ]
+    system_params  = [
+        #'/event_locator_param'
+    ]
+    
+    ## Defining arguments
+    args.update({'queue_size':      options.queue_size})
+    args.update({'latch':           options.latch})
+    args.update({'sub_topics':      sub_topics})
+    args.update({'pub_topics':      pub_topics})
+    #args.update({'system_params':   system_params})
+    
+    # Go to class functions that do all the heavy lifting.
+    try:
+        spinner = Sample(**args)
+    except rospy.ROSInterruptException:
+        pass
+    # Allow ROS to go to all callbacks.
+    rospy.spin()
+
